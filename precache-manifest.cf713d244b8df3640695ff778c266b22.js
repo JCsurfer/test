a4c89b138e9838fd4f16ad7cd1598360472e2be4
Juan Carlos Vidal <juancarvp@gmail.com>
2019-12-04 19:24:28 -0500

    Updates

---
+++ precache-manifest.cf713d244b8df3640695ff778c266b22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b898a22bf63f072c2608545dd79fa23b",
+    "url": "/JCsurfer/test.git/index.html"
+  },
+  {
+    "revision": "a47e4ad965e5c30b6c28",
+    "url": "/JCsurfer/test.git/static/css/main.cff729f3.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/JCsurfer/test.git/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "a47e4ad965e5c30b6c28",
+    "url": "/JCsurfer/test.git/static/js/main.05e29e71.chunk.js"
+  },
+  {
+    "revision": "4bcb0495976db0063188",
+    "url": "/JCsurfer/test.git/static/js/runtime~main.c3d4202e.js"
+  }
+]);
